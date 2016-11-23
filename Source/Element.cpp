9edd7b3728fbfc5bb1f2bb6ce20e3d50fb802ecf
PereWoulpy <laurent.turpin@insa-lyon.fr>
2016-11-23 20:13:40 +0100

    Modifications qui seront validées : 	nouveau fichier: Source/Catalogue.cpp 	nouveau fichier: Source/Catalogue.h 	nouveau fichier: Source/Element.cpp 	nouveau fichier: Source/Element.h 	nouveau fichier: Source/Makefile 	nouveau fichier: Source/Test.cpp 	nouveau fichier: Source/Trajet.cpp 	nouveau fichier: Source/Trajet.h 	nouveau fichier: Source/TrajetCompose.cpp 	nouveau fichier: Source/TrajetCompose.h 	nouveau fichier: Source/TrajetSimple.cpp 	nouveau fichier: Source/TrajetSimple.h

---
+++ Source/Element.cpp
@@ -0,0 +1,93 @@
+/*************************************************************************
+                           Element  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <Element> (fichier Element.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+using namespace std;
+#include <iostream>
+
+//------------------------------------------------------ Include personnel
+#include "Element.h"
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type Element::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+/*void Element::Ajouter(Element* lTr)
+{
+	next = lTr;
+}
+*/
+Trajet* Element::getTraj()
+{
+	return traj;
+}
+
+void Element::setNext(Element* n) 
+{
+	next = n;
+}
+
+Element* Element::getNext()
+{
+	return next;
+}
+
+
+//-------------------------------------------- Constructeurs - destructeur
+Element::Element ( const Element & unElement )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de copie de <Element>" << endl;
+#endif
+} //----- Fin de Element (constructeur de copie)
+
+
+Element::Element (Trajet * tr)
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au constructeur de <Element>" << endl;
+#endif
+
+traj = tr;
+next = NULL;
+
+} //----- Fin de Element
+
+
+Element::~Element ( )
+// Algorithme :
+//
+{
+#ifdef MAP
+    cout << "Appel au destructeur de <Element>" << endl;
+#endif
+
+delete traj;
+
+} //----- Fin de ~Element
+
+
+//------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
---
+++ Source/Trajet.h
@@ -0,0 +1,76 @@
+/*************************************************************************
+                           Trajet  -  description
+                             -------------------
+    début                : $DATE$
+    copyright            : (C) $YEAR$ par $AUTHOR$
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface de la classe <Trajet> (fichier Trajet.h) ----------------
+#if ! defined ( TRAJET_H )
+#define TRAJET_H
+
+//--------------------------------------------------- Interfaces utilisées
+
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types  
+
+//------------------------------------------------------------------------
+// Rôle de la classe <Trajet>
+//
+//
+//------------------------------------------------------------------------
+
+class Trajet
+{
+//----------------------------------------------------------------- PUBLIC
+
+public:
+//----------------------------------------------------- Méthodes publiques
+	virtual void Afficher () = 0; 
+    // Mode d'emploi : Affiche l'objet Trajet.
+    //
+    // Contrat : L'affichage tiens compte de la nature de l'objet, composé
+    // ou simple. 
+    
+    virtual const char* getDepart () = 0;
+    // Mode d'emploi : renvoie la ville de départ du trajet.
+    
+    virtual const char* getArrivee () = 0;
+    // Mode d'emploi : renvoie la ville d'arrivée du trajet.
+
+
+
+//-------------------------------------------- Constructeurs - destructeur
+    Trajet ( const Trajet & unTrajet );
+    // Mode d'emploi (constructeur de copie) :
+    //
+    // Contrat :
+    //
+
+    Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+    virtual ~Trajet ( );
+    // Mode d'emploi :
+    //
+    // Contrat :
+    //
+
+//------------------------------------------------------------------ PRIVE
+
+protected:
+//----------------------------------------------------- Méthodes protégées
+
+//----------------------------------------------------- Attributs protégés
+
+};
+
+//-------------------------------- Autres définitions dépendantes de <Trajet>
+
+#endif // TRAJET_H
+
